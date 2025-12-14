1c09283b36abdb14879f5b39f046880a436339e8
贾心悦 <jiaixinyue@laiye.com>
2025-12-14 23:50:10 +0800

    note 安卓打包优化

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,14 +5,14 @@ allprojects {
     }
 }
 buildscript {
-    val kotlin_version by extra("1.8.10")
+    val kotlin_version by extra("2.1.0")
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
